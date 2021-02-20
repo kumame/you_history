b05d2a056c9f400d863cc9ebd17ab2378df3cae9
kumame <enchant@vip.qq.com>
2021-02-20 17:30:06 +0800

    up

---
+++ info.js
@@ -0,0 +1,3 @@
+function load(){
+    document.getElementById('info-f').innerHTML="<h3><a href='https://kumame.github.io/you_history/'>You History Archive</a> Website create in 2021/02/20 , Maintainer <a href='http://www.kumame.pw/'>熊豆(kumame)</a></h3>";
+}
